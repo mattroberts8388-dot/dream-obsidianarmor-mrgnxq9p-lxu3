7cece571834917d99403fdee12d877c9ba28cab7
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-11 13:53:22 -0400

    Add src/main/java/com/obsidianarmor/ObsidianArmorModClient.java

---
+++ src/main/java/com/obsidianarmor/ObsidianArmorModClient.java
@@ -0,0 +1,9 @@
+package com.obsidianarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ObsidianArmorModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
