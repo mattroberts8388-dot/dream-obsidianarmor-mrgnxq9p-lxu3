a63fcda67d83fb8c7f20b47d295fe3067d861dea
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-11 13:53:21 -0400

    Add src/main/java/com/obsidianarmor/ObsidianArmorMaterial.java

---
+++ src/main/java/com/obsidianarmor/ObsidianArmorMaterial.java
@@ -0,0 +1,55 @@
+package com.obsidianarmor;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class ObsidianArmorMaterial implements ArmorMaterial {
+    public static final ObsidianArmorMaterial INSTANCE = new ObsidianArmorMaterial();
+
+    private static final int[] BASE_DURABILITY = {13, 15, 16, 11};
+    private static final int[] PROTECTION = {3, 6, 8, 3};
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY[type.getEquipmentSlot().getEntitySlotId()] * 40;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return PROTECTION[type.getEquipmentSlot().getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 10;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ObsidianArmorMod.OBSIDIAN_INGOT);
+    }
+
+    @Override
+    public String getName() {
+        return "obsidian";
+    }
+
+    @Override
+    public float getToughness() {
+        return 3.0f;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.2f;
+    }
+}
